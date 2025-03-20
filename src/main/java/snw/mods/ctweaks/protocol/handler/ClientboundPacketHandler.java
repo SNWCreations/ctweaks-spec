aa9c4aaf9778986ce3595a74fdd2ea2f022e67b3
ZX夏夜之风 <snwcreations@qq.com>
2025-03-20 20:51:01 +0800

    feat: add clear renderer packet for remove all renderers on client

--- src/main/java/snw/mods/ctweaks/protocol/handler/ClientboundPacketHandler.java
+++ src/main/java/snw/mods/ctweaks/protocol/handler/ClientboundPacketHandler.java
@@ -2,11 +2,14 @@ package snw.mods.ctweaks.protocol.handler;
 
 import snw.lib.protocol.handler.PacketHandler;
 import snw.mods.ctweaks.protocol.packet.s2c.ClientboundAddRendererPacket;
+import snw.mods.ctweaks.protocol.packet.s2c.ClientboundClearRendererPacket;
 import snw.mods.ctweaks.protocol.packet.s2c.ClientboundRemoveRendererPacket;
 import snw.mods.ctweaks.protocol.packet.s2c.ClientboundUpdateTextRendererPacket;
 
 public interface ClientboundPacketHandler extends PacketHandler {
     void handleAddRenderer(ClientboundAddRendererPacket packet);
+
+    void handleClearRenderer(ClientboundClearRendererPacket packet);
 
     void handleRemoveRenderer(ClientboundRemoveRendererPacket packet);
 
---
+++ src/main/java/snw/mods/ctweaks/protocol/packet/s2c/ClientboundClearRendererPacket.java
@@ -0,0 +1,32 @@
+package snw.mods.ctweaks.protocol.packet.s2c;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import snw.lib.protocol.packet.Packet;
+import snw.mods.ctweaks.protocol.handler.ClientboundPacketHandler;
+
+public class ClientboundClearRendererPacket extends Packet<ClientboundPacketHandler> {
+    public static final String TYPE = "clear_renderer";
+
+    public ClientboundClearRendererPacket(String nonce) {
+        super(nonce);
+    }
+
+    public ClientboundClearRendererPacket(ByteArrayDataInput input) {
+        super(input);
+    }
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    @Override
+    protected void doSerialization(ByteArrayDataOutput output) {
+    }
+
+    @Override
+    public void handle(ClientboundPacketHandler handler) {
+        handler.handleClearRenderer(this);
+    }
+}
