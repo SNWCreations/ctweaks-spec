4f62525fbf4928348ff6fbf9db93b7217b8fc9f6
ZX夏夜之风 <snwcreations@qq.com>
2025-03-20 20:52:23 +0800

    refactor: move position methods to renderer interface

--- src/main/java/snw/mods/ctweaks/object/pos/PlanePositioned.java
+++
@@ -1,9 +0,0 @@
-package snw.mods.ctweaks.object.pos;
-
-public interface PlanePositioned {
-    PlanePosition getPosition();
-
-    void setPosition(int x, int y);
-
-    void setPosition(PlanePosition position);
-}
--- src/main/java/snw/mods/ctweaks/render/TextRenderer.java
+++ src/main/java/snw/mods/ctweaks/render/TextRenderer.java
@@ -3,16 +3,18 @@ package snw.mods.ctweaks.render;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import snw.mods.ctweaks.object.ObjectUpdater;
-import snw.mods.ctweaks.object.pos.PlanePositioned;
+import snw.mods.ctweaks.object.pos.PlanePosition;
 
 import static snw.mods.ctweaks.Keys.modKey;
 
-public interface TextRenderer extends Renderer, PlanePositioned {
+public interface TextRenderer extends Renderer {
     Key TYPE = modKey("text");
 
     default Key getType() {
         return TYPE;
     }
+
+    PlanePosition getPosition();
 
     Component getText();
 
@@ -20,5 +22,7 @@ public interface TextRenderer extends Renderer, PlanePositioned {
 
     interface Updater extends ObjectUpdater {
         void setText(Component text);
+
+        void setPosition(PlanePosition position);
     }
 }
