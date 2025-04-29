44a7377c422f492599ecb1caa9fd76770af3e159
ZX夏夜之风 <snwcreations@qq.com>
2025-04-29 17:17:59 +0800

    feat: convenient factory method for PlanePosition

--- src/main/java/snw/mods/ctweaks/object/pos/PlanePosition.java
+++ src/main/java/snw/mods/ctweaks/object/pos/PlanePosition.java
@@ -4,4 +4,8 @@ public record PlanePosition(int x, int y) {
     public interface Getter {
         PlanePosition getPosition();
     }
+
+    public static PlanePosition planePos(int x, int y) {
+        return new PlanePosition(x, y);
+    }
 }
