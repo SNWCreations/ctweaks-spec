b0c2ef105f650a142c4d7ef2827c309a84d91344
ZX夏夜之风 <snwcreations@qq.com>
2025-04-29 16:46:08 +0800

    feat: creation method for player face renderer

--- src/main/java/snw/mods/ctweaks/entity/Screen.java
+++ src/main/java/snw/mods/ctweaks/entity/Screen.java
@@ -2,15 +2,19 @@ package snw.mods.ctweaks.entity;
 
 import org.jetbrains.annotations.UnmodifiableView;
 import snw.mods.ctweaks.object.pos.PlanePosition;
+import snw.mods.ctweaks.render.PlayerFaceRenderer;
 import snw.mods.ctweaks.render.Renderer;
 import snw.mods.ctweaks.render.TextRenderer;
 
 import java.util.Collection;
+import java.util.UUID;
 
 public interface Screen {
     Player getOwner();
 
     TextRenderer addTextRenderer(PlanePosition position);
+
+    PlayerFaceRenderer addPlayerFaceRenderer(UUID target, PlanePosition position, float scale);
 
     @UnmodifiableView
     Collection<Renderer> getRenderers();
--- src/main/java/snw/mods/ctweaks/render/PlayerFaceRenderer.java
+++ src/main/java/snw/mods/ctweaks/render/PlayerFaceRenderer.java
@@ -1,12 +1,21 @@
 package snw.mods.ctweaks.render;
 
+import net.kyori.adventure.key.Key;
 import snw.mods.ctweaks.object.ObjectUpdater;
 import snw.mods.ctweaks.object.Scaled;
 import snw.mods.ctweaks.object.pos.PlanePosition;
 
 import java.util.UUID;
 
+import static snw.mods.ctweaks.Keys.modKey;
+
 public interface PlayerFaceRenderer extends Renderer, PlanePosition.Getter, Scaled {
+    Key TYPE = modKey("player_face");
+
+    @Override
+    default Key getType() {
+        return TYPE;
+    }
 
     UUID getTarget();
 
