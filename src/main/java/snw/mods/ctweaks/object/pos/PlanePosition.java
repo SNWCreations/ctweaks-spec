c1dac864e374dd58d52240a614cd7f244a67b7ad
ZX夏夜之风 <snwcreations@qq.com>
2025-05-02 09:43:50 +0800

    fix: plane position only accept positive values

--- src/main/java/snw/mods/ctweaks/object/pos/PlanePosition.java
+++ src/main/java/snw/mods/ctweaks/object/pos/PlanePosition.java
@@ -1,6 +1,17 @@
 package snw.mods.ctweaks.object.pos;
 
-public record PlanePosition(int x, int y) {
+import com.google.common.base.Preconditions;
+import org.jetbrains.annotations.Range;
+
+public record PlanePosition(
+        @Range(from = 0, to = Integer.MAX_VALUE) int x,
+        @Range(from = 0, to = Integer.MAX_VALUE) int y
+) {
+    public PlanePosition {
+        Preconditions.checkArgument(x > 0, "x must be positive");
+        Preconditions.checkArgument(y > 0, "y must be positive");
+    }
+
     public interface Getter {
         PlanePosition getPosition();
     }
